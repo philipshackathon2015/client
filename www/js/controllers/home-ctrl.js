7499e3caf1d9c1a090a9ba52caf99ed37faf6e56
J. Ky Marsh <jkymarsh@gmail.com>
2015-03-07 17:24:36 -0800

    add home timeline statuses from data

--- www/js/controllers/home-ctrl.js
+++ www/js/controllers/home-ctrl.js
@@ -1,4 +1,27 @@
 angular.module('HealthSweet.controllers')
     .controller('HomeCtrl', function($scope) {
         console.log('home');
+
+        $scope.statuses = [
+            {
+                timestamp: 'March 5, 2015',
+                text: 'Some text about the mood status report that was completed'
+            },
+            {
+                timestamp: 'March 5, 2015',
+                text: 'Some text about the mood status report that was completed'
+            },
+            {
+                timestamp: 'March 5, 2015',
+                text: 'Some text about the mood status report that was completed'
+            },
+            {
+                timestamp: 'March 5, 2015',
+                text: 'Some text about the mood status report that was completed'
+            },
+            {
+                timestamp: 'March 5, 2015',
+                text: 'Some text about the mood status report that was completed'
+            }
+        ];
     });
