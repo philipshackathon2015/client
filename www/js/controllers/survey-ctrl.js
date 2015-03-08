ca37f4b87238399e5cdb03070cff1b23867f6ca2
J. Ky Marsh <jkymarsh@gmail.com>
2015-03-07 20:56:43 -0800

    clean up survey hierarchy and views

--- www/js/controllers/survey-ctrl.js
+++ www/js/controllers/survey-ctrl.js
@@ -1,9 +1,26 @@
 angular.module('HealthSweet.controllers')
-    .controller('SurveyCtrl', function($scope, $stateParams, $ionicHistory) {
+    .controller('SurveyCtrl', function($scope, $state, $stateParams, $timeout, $ionicLoading) {
         console.log('survey');
         console.log($stateParams.timestamp);
 
         $scope.timestamp = $stateParams.timestamp;
 
-        $scope.formData = {};
+        $scope.formData = {
+            userId: 123,
+            timestamp: $stateParams.timestamp
+        };
+
+        $scope.processForm = function() {
+            console.log('PROCESS FORM');
+
+            $ionicLoading.show({
+                template: 'Sending survey...'
+            });
+
+            $timeout(function() {
+                $ionicLoading.hide();
+
+                $state.go('app.home');
+            }, 600);
+        };
     });
